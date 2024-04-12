6d508d56421eb13b6ed3868d37bdf8111d0dfb46
swerizwan <44375455+swerizwan@users.noreply.github.com>
2024-04-12 16:42:37 +0800

    main.py

--- render/main.py
+++ render/main.py
@@ -3,75 +3,94 @@ import os
 import numpy as np
 import sys
 
-# Function to set up Blender scene
-def setup_scene(root_dir, filename):
-    """
-    Sets up the Blender scene with appropriate settings.
+# Get filename and root directory from command line arguments
+filename = str(sys.argv[-1])
+root_dir = str(sys.argv[-2])
 
-    Args:
-        root_dir (str): Root directory of the project.
-        filename (str): Name of the file being processed.
+# List of blendshape names
+model_bsList = ["browDownLeft", "browDownRight", "browInnerUp", "browOuterUpLeft", "browOuterUpRight",
+                "cheekPuff", "cheekSquintLeft", "cheekSquintRight", "eyeBlinkLeft", "eyeBlinkRight",
+                "eyeLookDownLeft", "eyeLookDownRight", "eyeLookInLeft", "eyeLookInRight", "eyeLookOutLeft",
+                "eyeLookOutRight", "eyeLookUpLeft", "eyeLookUpRight", "eyeSquintLeft", "eyeSquintRight",
+                "eyeWideLeft", "eyeWideRight", "jawForward", "jawLeft", "jawOpen", "jawRight", "mouthClose",
+                "mouthDimpleLeft", "mouthDimpleRight", "mouthFrownLeft", "mouthFrownRight", "mouthFunnel",
+                "mouthLeft", "mouthLowerDownLeft", "mouthLowerDownRight", "mouthPressLeft", "mouthPressRight",
+                "mouthPucker", "mouthRight", "mouthRollLower", "mouthRollUpper", "mouthShrugLower",
+                "mouthShrugUpper", "mouthSmileLeft", "mouthSmileRight", "mouthStretchLeft", "mouthStretchRight",
+                "mouthUpperUpLeft", "mouthUpperUpRight", "noseSneerLeft", "noseSneerRight", "tongueOut"]
 
-    Returns:
-        tuple: A tuple containing the output directory and the path to the blendshape file.
-    """
+# Get the 'face' object
+obj = bpy.data.objects["face"]
+
+# Set Blender scene settings
+bpy.context.scene.render.engine = 'BLENDER_WORKBENCH'
+bpy.context.scene.display.shading.light = 'MATCAP'
+bpy.context.scene.display.render_aa = 'FXAA'
+bpy.context.scene.render.resolution_x = int(512)
+bpy.context.scene.render.resolution_y = int(768)
+bpy.context.scene.render.fps = 30
+bpy.context.scene.render.image_settings.file_format = 'PNG'
+
+def setup_blender_scene():
+    """Set up Blender scene settings."""
     bpy.context.scene.render.engine = 'BLENDER_WORKBENCH'
     bpy.context.scene.display.shading.light = 'MATCAP'
     bpy.context.scene.display.render_aa = 'FXAA'
-    bpy.context.scene.render.resolution_x = 512
-    bpy.context.scene.render.resolution_y = 768
+    bpy.context.scene.render.resolution_x = int(512)
+    bpy.context.scene.render.resolution_y = int(768)
     bpy.context.scene.render.fps = 30
     bpy.context.scene.render.image_settings.file_format = 'PNG'
 
+def set_camera_settings():
+    """Set camera settings."""
     cam = bpy.data.objects['Camera']
     cam.scale = [2, 2, 2]
     bpy.context.scene.camera = cam
 
-    output_dir = os.path.join(root_dir, filename)
-    blendshape_path = os.path.join(root_dir, filename + '.npy')
-    return output_dir, blendshape_path
+def render_frame(output_dir, filename, blendshape_data):
+    """Render frames for each blendshape."""
+    obj = bpy.data.objects["face"]
+    model_bsList = ["browDownLeft", "browDownRight", "browInnerUp", "browOuterUpLeft", "browOuterUpRight",
+                    "cheekPuff", "cheekSquintLeft", "cheekSquintRight", "eyeBlinkLeft", "eyeBlinkRight",
+                    "eyeLookDownLeft", "eyeLookDownRight", "eyeLookInLeft", "eyeLookInRight", "eyeLookOutLeft",
+                    "eyeLookOutRight", "eyeLookUpLeft", "eyeLookUpRight", "eyeSquintLeft", "eyeSquintRight",
+                    "eyeWideLeft", "eyeWideRight", "jawForward", "jawLeft", "jawOpen", "jawRight", "mouthClose",
+                    "mouthDimpleLeft", "mouthDimpleRight", "mouthFrownLeft", "mouthFrownRight", "mouthFunnel",
+                    "mouthLeft", "mouthLowerDownLeft", "mouthLowerDownRight", "mouthPressLeft", "mouthPressRight",
+                    "mouthPucker", "mouthRight", "mouthRollLower", "mouthRollUpper", "mouthShrugLower",
+                    "mouthShrugUpper", "mouthSmileLeft", "mouthSmileRight", "mouthStretchLeft", "mouthStretchRight",
+                    "mouthUpperUpLeft", "mouthUpperUpRight", "noseSneerLeft", "noseSneerRight", "tongueOut"]
 
-# Function to render images
-def render_images(obj, model_bsList, output_dir, bs):
-    """
-    Renders images for each blendshape.
-
-    Args:
-        obj (bpy.types.Object): The object representing the face model.
-        model_bsList (list): List of blendshape names.
-        output_dir (str): Output directory for rendered images.
-        bs (numpy.ndarray): Blendshape values for each frame.
-    """
-    for i, curr_bs in enumerate(bs):
+    # Iterate over blendshapes
+    for i, curr_bs in enumerate(blendshape_data):
+        # Set blendshape values for the 'face' object
         for j, value in enumerate(curr_bs):
             obj.data.shape_keys.key_blocks[model_bsList[j]].value = value
-        bpy.context.scene.render.filepath = os.path.join(output_dir, '{}.png'.format(i))
+        # Set output file path
+        bpy.context.scene.render.filepath = os.path.join(output_dir, f'{filename}_{i}.png')
+        # Render the frame
         bpy.ops.render.render(write_still=True)
 
-def main():
-    filename = str(sys.argv[-1])
-    root_dir = str(sys.argv[-2])
 
-    model_bsList = [
-        "browDownLeft", "browDownRight", "browInnerUp", "browOuterUpLeft", "browOuterUpRight",
-        "cheekPuff", "cheekSquintLeft", "cheekSquintRight", "eyeBlinkLeft", "eyeBlinkRight",
-        "eyeLookDownLeft", "eyeLookDownRight", "eyeLookInLeft", "eyeLookInRight", "eyeLookOutLeft",
-        "eyeLookOutRight", "eyeLookUpLeft", "eyeLookUpRight", "eyeSquintLeft", "eyeSquintRight",
-        "eyeWideLeft", "eyeWideRight", "jawForward", "jawLeft", "jawOpen", "jawRight", "mouthClose",
-        "mouthDimpleLeft", "mouthDimpleRight", "mouthFrownLeft", "mouthFrownRight", "mouthFunnel",
-        "mouthLeft", "mouthLowerDownLeft", "mouthLowerDownRight", "mouthPressLeft", "mouthPressRight",
-        "mouthPucker", "mouthRight", "mouthRollLower", "mouthRollUpper", "mouthShrugLower", "mouthShrugUpper",
-        "mouthSmileLeft", "mouthSmileRight", "mouthStretchLeft", "mouthStretchRight", "mouthUpperUpLeft",
-        "mouthUpperUpRight", "noseSneerLeft", "noseSneerRight", "tongueOut"
-    ]
+# Set camera settings
+cam = bpy.data.objects['Camera']
+cam.scale = [2, 2, 2]
+bpy.context.scene.camera = cam
 
-    obj = bpy.data.objects["face"]
+# Output directory
+output_dir = root_dir + filename
+blendshape_path = root_dir + filename + '.npy'
 
-    output_dir, blendshape_path = setup_scene(root_dir, filename)
+result = []
+bs = np.load(blendshape_path)
 
-    bs = np.load(blendshape_path)
-
-    render_images(obj, model_bsList, output_dir, bs)
-
-if __name__ == "__main__":
-    main()
+# Iterate over blendshapes
+for i in range(bs.shape[0]):
+    curr_bs = bs[i]
+    # Set blendshape values for the 'face' object
+    for j in range(52):
+        obj.data.shape_keys.key_blocks[model_bsList[j]].value = curr_bs[j]
+    # Set output file path
+    bpy.context.scene.render.filepath = os.path.join(output_dir, '{}.png'.format(i))
+    # Render the frame
+    bpy.ops.render.render(write_still=True)
